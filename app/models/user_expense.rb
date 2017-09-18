b243505ee3694e83c2b10781847cb6300d97ba90
Hawken Ritter <hawkenritter@gmail.com>
2017-09-17 18:38:36 -0700

    Create migrations and associations

--- app/models/user_expense.rb
+++ app/models/user_expense.rb
@@ -1,2 +1,5 @@
 class UserExpense < ApplicationRecord
+  validates :user_id, :expense_id, :expense_amount, presence: true
+  belongs_to :expense
+  belongs_to :user
 end
--- db/migrate/20170917221842_create_user_trips.rb
+++ db/migrate/20170917221842_create_user_trips.rb
@@ -1,6 +1,8 @@
 class CreateUserTrips < ActiveRecord::Migration[5.1]
   def change
     create_table :user_trips do |t|
+      t.integer :user_id
+      t.integer :trip_id
 
       t.timestamps
     end
